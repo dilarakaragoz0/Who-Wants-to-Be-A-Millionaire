42e1aab80424375ddd17488526f12fd60553afdf
dilarakaragoz0 <dilarakaragoz71@gmail.com>
2023-03-31 12:37:11 +0300

    Add project files.

---
+++ Who Wants to Be A Millionaire/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Who_Wants_to_Be_A_Millionaire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
